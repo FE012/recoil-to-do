46512320da79cb3b8e40337a1dd08e5f89a7668d
FE012 <twte96@gmail.com>
2023-08-30 22:29:13 +0900

    feat: Todo 수정기능 구현

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -6,6 +6,8 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 import { faListUl } from "@fortawesome/free-solid-svg-icons";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faWrench } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Li = styled.li`
   border: 1px solid #769fcd;
@@ -14,25 +16,37 @@ const Li = styled.li`
   display: flex;
   flex-direction: column;
   padding: 10px;
-  div {
-    margin-top: 10px;
-    display: flex;
-    justify-content: end;
-    button {
-      background-color: #769fcd;
-      border: none;
-      border-radius: 5px;
-      width: 80px;
-      height: 30px;
-      margin-right: 5px;
-      font-family: "Cafe24SsurroundAir";
-    }
+  button {
+    background-color: #769fcd;
+    border: none;
+    border-radius: 5px;
+    width: 80px;
+    height: 30px;
+    margin-right: 5px;
+    font-family: "Cafe24SsurroundAir";
   }
 `;
 
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  button:hover {
+    cursor: pointer;
+  }
+`;
+
+const TextArea = styled.textarea`
+  font-family: "Cafe24SsurroundAir";
+  border: none;
+  background-color: ${(props) => props.theme.bgColor};
+  outline: none;
+  transition: width 0.2s ease-in-out; /* Smooth transition effect */
+`;
+
 function ToDo({ text, category, id }: IToDo) {
-  const setToDos = useSetRecoilState(toDoState);
   // console.log("props로 받은 id:", id);
+  const setToDos = useSetRecoilState(toDoState);
+  const [areaText, setareaText] = useState(text);
 
   //newCategory: IToDo["category"]
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -61,11 +75,31 @@ function ToDo({ text, category, id }: IToDo) {
     });
   };
 
+  //toDoList 수정버튼 event
+  const handleUpdateClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text: areaText, id, category };
+
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+
   return (
     <Li>
-      <span>{text}</span>
       {/* 카테고리가 "DOING"이 아닐 때만, Doing 버튼을 보여줄 거임  */}
-      <div>
+      <TextArea
+        value={areaText}
+        onChange={(e) => setareaText(e.currentTarget.value)}
+        rows={8}
+        cols={40}
+        style={{ resize: "vertical" }}
+      />
+      <ButtonBox>
         {category !== Categories.DOING && (
           <button name={Categories.DOING} onClick={onClick}>
             진행 중{" "}
@@ -87,11 +121,15 @@ function ToDo({ text, category, id }: IToDo) {
             />
           </button>
         )}
+        <button onClick={handleUpdateClick}>
+          수정완료{" "}
+          <FontAwesomeIcon icon={faWrench} style={{ color: "#9F0D7F" }} />
+        </button>
         <button onClick={handleDeleteClick}>
           삭제{" "}
           <FontAwesomeIcon icon={faTrashCan} style={{ color: "#F6BA6F" }} />
         </button>
-      </div>
+      </ButtonBox>
     </Li>
   );
 }
